4b3fe381321d6370a2131ac13b36a5c0513364ea
echo <614699596@qq.com>
2025-07-08 14:39:59 +0800

    5. 内存检测

--- src/loader.asm
+++ src/loader.asm
@@ -6,6 +6,55 @@ dw 0x55aa
 mov si, booting             ; si 寄存器通常用于字符串操作
 call print
 
+detect_memory:
+    ; 1. 将 ebx 置为 0
+    xor ebx, ebx
+
+    ; 2. es:di 结构体的缓存位置: 指定BIOS将ards写到这里
+    mov ax, 0
+    mov es, ax
+    mov edi, ards_buffer
+
+    ; 3. 固定为签名标记 `0x534d4150`，此十六进制数字是字符串 `SMAP` 的ASCII 码
+    mov edx, 0x534d4150; 固定签名
+
+.next:
+    ; 子功能号
+    mov eax, 0xe820
+    ; ards 结构的大小 (字节)
+    mov ecx, 20
+    ; 4. 调用 0x15 系统调用
+    int 0x15
+
+    ; CF：0 success 1 failed
+    jc error
+
+    ; 将缓存指针指向下一个结构体
+    add di, cx
+
+    ; 将结构体数量加一
+    inc word [ards_count]
+
+    ; 0表示最后一个ards
+    cmp ebx, 0
+    jnz .next
+
+; 完成内存检测
+.success:
+    mov si, detecting
+    call print
+
+    ; 结构体数量
+    mov cx, [ards_count]
+    ; 结构体指针
+    mov si, 0
+
+.show:
+    mov eax, [ards_buffer + si]
+    mov ebx, [ards_buffer + si + 8]
+    mov edx, [ards_buffer + si + 16]
+    add si, 20
+    loop .show
 jmp $
 
 print:
@@ -20,6 +69,18 @@ print:
 .done:
     ret
 
-; 存放booting字符串
+error:
+    mov si, .msg
+    call print
+    hlt; 让 CPU 停止
+    jmp $
+    .msg db "Loading Error!!!", 10, 13, 0
+
 booting:
     db "Loading OS...", 10, 13, 0 ; \n\r
+detecting:
+    db "Detecting Memory Success...", 10, 13, 0; \n\r
+
+ards_count:
+    dw 0
+ards_buffer:
